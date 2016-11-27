76c0a42684b6bf7a542732fd5a060fc47bd3cd3b
Keridos <2742845+Keridos@users.noreply.github.com>
2016-11-27 22:56:54 +0100

    added abstract block classes

---
+++ src/main/java/omtteam/omlib/blocks/BlockAbstract.java
@@ -0,0 +1,23 @@
+package omtteam.omlib.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+
+/**
+ * Created by Keridos on 05/12/2015.
+ * This Class
+ */
+public abstract class BlockAbstract extends Block {
+    protected BlockAbstract(Material material) {
+        super(material);
+    }
+
+    @Override
+    public boolean canCreatureSpawn(IBlockState state, IBlockAccess world, BlockPos pos, EntityLiving.SpawnPlacementType type) {
+        return false;
+    }
+}
