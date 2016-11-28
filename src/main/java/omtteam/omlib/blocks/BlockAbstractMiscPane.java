347239cb1f48c1a52e0c6977b7858437a840688d
Keridos <2742845+Keridos@users.noreply.github.com>
2016-11-28 11:04:39 +0100

    a little more work towards fences/tile entitites

--- src/main/java/omtteam/omlib/blocks/BlockAbstractMiscPane.java
+++ src/main/java/omtteam/omlib/blocks/BlockAbstractMiscPane.java
@@ -4,8 +4,10 @@ import net.minecraft.block.BlockPane;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 
 
 /**
@@ -16,6 +18,14 @@ public abstract class BlockAbstractMiscPane extends BlockPane {
     protected BlockAbstractMiscPane(String par1, String par2, Material material, boolean par4) {
         super(material, par4);
     }
+
+    @Override
+    public boolean hasTileEntity(IBlockState state) {
+        return true;
+    }
+
+    @Override
+    public abstract TileEntity createTileEntity(World world, IBlockState state);
 
     @Override
     public boolean canCreatureSpawn(IBlockState state, IBlockAccess world, BlockPos pos, EntityLiving.SpawnPlacementType type) {
