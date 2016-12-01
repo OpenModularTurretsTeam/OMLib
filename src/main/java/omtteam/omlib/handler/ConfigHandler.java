e4006de548a1c60dfae68047fd81538af2de95c0
Keridos <fuffzehn@gmail.com>
2016-12-01 02:19:25 +0100

    fixing config

--- src/main/java/omtteam/omlib/handler/ConfigHandler.java
+++ src/main/java/omtteam/omlib/handler/ConfigHandler.java
@@ -10,22 +10,15 @@ public class ConfigHandler {
     public static boolean EUSupport;
     public static double EUtoRFRatio;
     private static int potentiaToRFRatio;
-    private static int potentiaAddonCapacity;
 
     public static void init(File configFile) {
         Configuration config = new Configuration(configFile);
         config.load();
 
-        potentiaToRFRatio = config.get("ModCompatibility", "Potentia Addons' RF conversion ratio per 1 essentia",
-                                       500).getInt();
-
-        potentiaAddonCapacity = config.get("ModCompatibility", "How much essentia the Potentia Addon can store",
-                                           20).getInt();
-
-
-        EUSupport = config.get("ModCompatability", "Can turrets be powered with EU?", true).getBoolean();
-        offlineModeSupport = config.get("ModCompatability", "Enable offline mode support?(warning, makes turrets fairly unsafe)", false).getBoolean();
-        EUtoRFRatio = config.get("ModCompatability", "EU to RF Ratio", 4.0D).getDouble();
+        potentiaToRFRatio = config.get("ModCompatibility", "Potentia to RF conversion ratio per essentia", 500).getInt();
+        EUSupport = config.get("ModCompatibility", "Can turrets be powered with EU?", true).getBoolean();
+        offlineModeSupport = config.get("General", "Enable offline mode support?(warning, makes turrets fairly unsafe)", false).getBoolean();
+        EUtoRFRatio = config.get("ModCompatibility", "EU to RF Ratio", 4.0D).getDouble();
 
         if (config.hasChanged()) {
             config.save();
