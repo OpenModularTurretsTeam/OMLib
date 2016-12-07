81032ae5e0f5e7431ebfefc48844876058f7bc54
Keridos <2742845+Keridos@users.noreply.github.com>
2016-12-07 23:25:50 +0100

    fixing reflection and removed some debug code

--- src/main/java/omtteam/omlib/util/InvUtil.java
+++ src/main/java/omtteam/omlib/util/InvUtil.java
@@ -4,8 +4,6 @@ import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 
-import java.util.logging.Logger;
-
 /**
  * Created by Keridos on 03/12/16.
  * This Class contains a modified version of the mergeItemStack so that it respect slot.getItemStackLimit(stack) and
@@ -39,7 +37,6 @@ public class InvUtil {
                 {
                     int j = itemstack.stackSize + stack.stackSize;
 
-                    Logger.getGlobal().info (" stacklimit: "+slot.getItemStackLimit(stack));
                     if (j <= Math.min(stack.getMaxStackSize(), slot.getItemStackLimit(stack)))
                     {
                         stack.stackSize = 0;
