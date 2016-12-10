b6e4e9ecb75bc2fb748e1033f1d1c12d1b3cfe0b
Keridos <2742845+Keridos@users.noreply.github.com>
2016-12-10 17:26:42 +0100

    GUI and localization updates, also first version of the memory card.

--- src/main/java/omtteam/omlib/reference/Names.java
+++ src/main/java/omtteam/omlib/reference/Names.java
@@ -11,5 +11,11 @@ public class Names {
         public static final String ACTIVE = "gui.omtteam.omlib:active";
         public static final String TRUE = "gui.omtteam.omlib:true";
         public static final String FALSE = "gui.omtteam.omlib:false";
+        public static final String YES = "gui.omtteam.omlib:yes";
+        public static final String NO = "gui.omtteam.omlib:no";
+        public static final String TRUSTED_PLAYERS = "gui.omtteam.omlib:trusted_players";
+        public static final String ATTACK_PLAYERS = "gui.omtteam.omlib:attack_players";
+        public static final String ATTACK_MOBS = "gui.omtteam.omlib:attack_mobs";
+        public static final String ATTACK_NEUTRALS = "gui.omtteam.omlib:attack_neutrals";;
     }
 }
--- src/main/java/omtteam/omlib/util/GeneralUtil.java
+++ src/main/java/omtteam/omlib/util/GeneralUtil.java
@@ -4,6 +4,7 @@ import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.translation.I18n;
 import net.minecraftforge.fml.common.registry.GameData;
+import omtteam.omlib.reference.Names;
 
 /**
  * Created by Keridos on 17.05.2015.
@@ -21,6 +22,32 @@ public class GeneralUtil {
             return I18n.translateToLocal(text);
         } else {
             return I18n.translateToFallback(text);
+        }
+    }
+
+    public static String getBooleanLocalization(boolean bool) {
+        String localization = (bool ? Names.Localizations.TRUE : Names.Localizations.FALSE);
+        if (!I18n.translateToLocal(localization).equals(localization)) {
+            return I18n.translateToLocal(localization);
+        } else {
+            return I18n.translateToFallback(localization);
+        }
+    }
+
+    public static String getColoredBooleanLocalization(boolean bool) {
+        String localization = (bool ? Names.Localizations.TRUE : Names.Localizations.FALSE);
+        if (!I18n.translateToLocal(localization).equals(localization)) {
+            return (bool ? "\u00A72" : "\u00A74") + I18n.translateToLocal(localization);
+        } else {
+            return (bool ? "\u00A72" : "\u00A74") + I18n.translateToFallback(localization);
+        }
+    }
+    public static String getColoredBooleanLocalizationYesNo(boolean bool) {
+        String localization = (bool ? Names.Localizations.YES : Names.Localizations.NO);
+        if (!I18n.translateToLocal(localization).equals(localization)) {
+            return (bool ? "\u00A72" : "\u00A74") + I18n.translateToLocal(localization);
+        } else {
+            return (bool ? "\u00A72" : "\u00A74") + I18n.translateToFallback(localization);
         }
     }
 }
