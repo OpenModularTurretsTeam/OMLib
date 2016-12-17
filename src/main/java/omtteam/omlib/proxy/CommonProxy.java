b2695d1288c6a9e7f389bfecaee5bfbc6a0dd56d
Keridos <2742845+Keridos@users.noreply.github.com>
2016-12-17 02:21:21 +0100

    Annotations and a bit of cleanup

--- src/main/java/omtteam/omlib/proxy/CommonProxy.java
+++ src/main/java/omtteam/omlib/proxy/CommonProxy.java
@@ -1,5 +1,6 @@
 package omtteam.omlib.proxy;
 
+@SuppressWarnings({"EmptyMethod", "WeakerAccess", "unused"})
 public class CommonProxy {
     public void preInit(){
 
--- src/main/java/omtteam/omlib/tileentity/TileEntityContainer.java
+++ src/main/java/omtteam/omlib/tileentity/TileEntityContainer.java
@@ -14,6 +14,7 @@ import javax.annotation.ParametersAreNonnullByDefault;
  * Created by Keridos on 05/12/2015.
  * This Class
  */
+@SuppressWarnings("WeakerAccess")
 public abstract class TileEntityContainer extends TileEntityOwnedBlock implements ISidedInventory {
     protected ItemStack[] inventory;
 
