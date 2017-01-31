ad93c4e137ef668819407dfd0f80fca2d3215ec0
Keridos <2742845+Keridos@users.noreply.github.com>
2017-01-31 07:30:56 +0100

    fixing a crash with tier 5 turret bases

---
+++ src/main/java/omtteam/omlib/render/CamoBakedModel.java
@@ -0,0 +1,85 @@
+package omtteam.omlib.render;
+
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
+import net.minecraft.client.renderer.block.model.ItemOverrideList;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.property.IExtendedBlockState;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import omtteam.omlib.blocks.BlockAbstractCamoTileEntity;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.List;
+
+import static omtteam.omlib.blocks.BlockAbstractCamoTileEntity.RENDERBLOCKSTATE;
+
+
+/**
+ * Created by Keridos on 29/01/17.
+ * This Class is the abstract implementation of the camo model.
+ * Extending classes must implement 3 additional classes that manage the default models caching and statemapping.
+ * See TurretBaseBakedModel in OMT for a reference implementation.
+ */
+public abstract class CamoBakedModel implements IBakedModel {
+    protected static ResourceLocation FAKE_LOCATION;
+    private List<IBakedModel> defaultModels;
+
+    public CamoBakedModel(List<IBakedModel> list) {
+        defaultModels = list;
+    }
+
+    protected abstract IBakedModel getModel(List<IBakedModel> list, IBlockState state);
+
+    @Override
+    @Nonnull
+    @SideOnly(Side.CLIENT)
+    public List<BakedQuad> getQuads(@Nullable IBlockState state, @Nullable EnumFacing side, long rand) {
+        IExtendedBlockState extendedState = (IExtendedBlockState) state;
+        IBlockState camoState = extendedState.getValue(RENDERBLOCKSTATE).getRenderState();
+
+        if (camoState.getBlock() instanceof BlockAbstractCamoTileEntity && state != null) {
+            return getModel(defaultModels, state).getQuads(state, side, rand);
+
+        } else {
+            return Minecraft.getMinecraft().getBlockRendererDispatcher().getBlockModelShapes().getModelForState(camoState).getQuads(camoState, side, rand);
+        }
+    }
+
+    @Override
+    public boolean isAmbientOcclusion() {
+        return true;
+    }
+
+    @Override
+    public boolean isGui3d() {
+        return false;
+    }
+
+    @Override
+    public boolean isBuiltInRenderer() {
+        return false;
+    }
+
+    @Override
+    public TextureAtlasSprite getParticleTexture() {
+        return null;
+    }
+
+    @Override
+    public ItemCameraTransforms getItemCameraTransforms() {
+        return null;
+    }
+
+    @Override
+    public ItemOverrideList getOverrides() {
+        return null;
+    }
+}
