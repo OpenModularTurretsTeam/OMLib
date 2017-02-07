aa387919f0134a7ea329e21014f167ae66a3a003
Keridos <2742845+Keridos@users.noreply.github.com>
2017-02-07 11:49:23 +0100

    next step to multi mc version compatability

--- src/main/java/omtteam/omlib/compatability/minecraft/CompatPane.java
+++ src/main/java/omtteam/omlib/compatability/minecraft/CompatPane.java
@@ -13,7 +13,6 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -35,11 +34,11 @@ public class CompatPane extends BlockPane {
     }
 
     protected void clAddCollisionBoxToList(IBlockState state, World world, BlockPos pos, AxisAlignedBB entityBox, List<AxisAlignedBB> list, Entity entity) {
-        super.addCollisionBoxToList(state, world, pos, entityBox, list, entity, false);
+        super.addCollisionBoxToList(state, world, pos, entityBox, list, entity);
     }
 
     @Override
-    public void addCollisionBoxToList(IBlockState state, World worldIn, BlockPos pos, AxisAlignedBB entityBox, List<AxisAlignedBB> collidingBoxes, @Nullable Entity entityIn, boolean p_185477_7_) {
+    public void addCollisionBoxToList(IBlockState state, World worldIn, BlockPos pos, AxisAlignedBB entityBox, List<AxisAlignedBB> collidingBoxes, @Nullable Entity entityIn) {
         clAddCollisionBoxToList(state, worldIn, pos, entityBox, collidingBoxes, entityIn);
     }
 
@@ -48,7 +47,7 @@ public class CompatPane extends BlockPane {
     }
 
     @Override
-    public void neighborChanged(IBlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos p_189540_5_) {
+    public void neighborChanged(IBlockState state, World worldIn, BlockPos pos, Block blockIn) {
         clOnNeighborChanged(state, worldIn, pos, blockIn);
     }
 
@@ -57,35 +56,35 @@ public class CompatPane extends BlockPane {
     }
 
     @Override
-    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, @Nullable ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ) {
         return clOnBlockActivated(worldIn, pos, state, playerIn, hand, side, hitX, hitY, hitZ);
     }
 
-    @Override
-    public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
-        return clGetStateForPlacement(worldIn, pos, facing, hitX, hitY, hitZ, meta, placer);
+    protected IBlockState clGetStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
+        return super.onBlockPlaced(worldIn, pos, facing, hitX, hitY, hitZ, meta, placer);
     }
 
-    protected IBlockState clGetStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
-        return super.getStateForPlacement(worldIn, pos, facing, hitX, hitY, hitZ, meta, placer);
+    @Override
+    public IBlockState onBlockPlaced(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
+        return clGetStateForPlacement(worldIn, pos, facing, hitX, hitY, hitZ, meta, placer);
     }
 
     @SideOnly(Side.CLIENT)
     @Override
-    public void getSubBlocks(Item itemIn, CreativeTabs tab, NonNullList<ItemStack> subItems) {
+    public void getSubBlocks(Item itemIn, CreativeTabs tab, List<ItemStack> subItems) {
         clGetSubBlocks(itemIn, tab, subItems);
     }
 
     @SideOnly(Side.CLIENT)
     protected void clGetSubBlocks(Item itemIn, CreativeTabs tab, List<ItemStack> subItems) {
-        super.getSubBlocks(itemIn, tab, (NonNullList<ItemStack>) subItems);
+        super.getSubBlocks(itemIn, tab, subItems);
     }
 
     public static boolean activateBlock(Block block, World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-        return block.onBlockActivated(world, pos, state, player, hand, facing, hitX, hitY, hitZ);
+        return block.onBlockActivated(world, pos, state, player, hand, player.getHeldItem(hand), facing, hitX, hitY, hitZ);
     }
 
     public static Collection<IProperty<?>> getPropertyKeys(IBlockState state) {
-        return state.getPropertyKeys();
+        return state.getPropertyNames();
     }
 }
--- src/main/java/omtteam/omlib/util/compat/WorldTools.java
+++ src/main/java/omtteam/omlib/util/compat/WorldTools.java
@@ -1,6 +1,8 @@
 package omtteam.omlib.util.compat;
 
 import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.WorldClient;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.EnumFacing;
@@ -14,30 +16,34 @@ import java.util.List;
 public class WorldTools {
 
     public static void spawnEntity(World world, Entity entity) {
-        world.spawnEntity(entity);
+        world.spawnEntityInWorld(entity);
+    }
+
+    public static WorldClient getWorld(Minecraft mc) {
+        return mc.theWorld;
     }
 
     public static void neighborChanged(World world, EnumFacing offset, Block block, BlockPos pos) {
-        world.neighborChanged(pos.offset(offset), block, pos);
+        world.notifyBlockOfStateChange(pos.offset(offset), block);
     }
 
     public static void notifyBlockOfStateChange(World world, BlockPos pos, Block block, BlockPos fromPos) {
-        world.neighborChanged(pos, block, fromPos);
+        world.notifyBlockOfStateChange(pos, block);
     }
 
     public static void notifyNeighborsOfStateChange(World world, BlockPos pos, Block block) {
-        world.notifyNeighborsOfStateChange(pos, block, false);
+        world.notifyNeighborsOfStateChange(pos, block);
     }
 
     public static List<EntityPlayerMP> getPlayerList(WorldServer world) {
-        return world.getMinecraftServer().getPlayerList().getPlayers();
+        return world.getMinecraftServer().getPlayerList().getPlayerList();
     }
 
     public static <T extends WorldSavedData> T loadData(World world, Class<? extends T> clazz, String dataID) {
-        return (T) world.loadData(clazz, dataID);
+        return (T) world.loadItemData(clazz, dataID);
     }
 
     public static <T extends WorldSavedData> void saveData(World world, String dataID, T data) {
-        world.setData(dataID, data);
+        world.setItemData(dataID, data);
     }
 }
