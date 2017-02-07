c24ffad6362ca595178929ef5df372ae515d8882
Keridos <2742845+Keridos@users.noreply.github.com>
2017-02-07 10:34:06 +0100

    some testing can be done now!

---
+++ src/main/java/omtteam/omlib/util/compat/ChatTools.java
@@ -0,0 +1,22 @@
+package omtteam.omlib.util.compat;
+
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.text.ITextComponent;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Created by Keridos on 07/02/17.
+ * This Class
+ */
+public class ChatTools {
+
+    public static void addChatMessage(@Nonnull ICommandSender sender, @Nonnull ITextComponent component) {
+        if (sender instanceof EntityPlayer) {
+            ((EntityPlayer) sender).sendStatusMessage(component, false);
+        } else {
+            sender.sendMessage(component);
+        }
+    }
+}
---
+++ src/main/java/omtteam/omlib/util/compat/CompatCommandBase.java
@@ -0,0 +1,16 @@
+package omtteam.omlib.util.compat;
+
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+
+public abstract class CompatCommandBase extends CommandBase {
+
+    public static boolean canUseCommand(ICommandSender sender, int permLevel, String name) {
+        return sender.canUseCommand(permLevel, name);
+    }
+
+    public static String getCommandName(ICommand command) {
+        return command.getName();
+    }
+}
