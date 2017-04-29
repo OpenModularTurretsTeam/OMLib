cd19ff65510d4f8f77c03719318a307a9fba0a2a
Keridos <fuffzehn@gmail.com>
2017-04-29 04:08:26 +0200

    removed cofh api from distribution, fix #314 from OMT

--- src/main/java/omtteam/omlib/power/tesla/BaseOMTeslaContainerWrapper.java
+++ src/main/java/omtteam/omlib/power/tesla/BaseOMTeslaContainerWrapper.java
@@ -2,12 +2,14 @@ package omtteam.omlib.power.tesla;
 
 import net.darkhax.tesla.api.ITeslaConsumer;
 import net.minecraft.util.EnumFacing;
+import net.minecraftforge.energy.CapabilityEnergy;
+import omtteam.omlib.power.OMEnergyStorage;
 import omtteam.omlib.tileentity.TileEntityElectric;
 
 /**
  * A Receiver for Energy, credits go to the ender IO devs :)
  */
-@SuppressWarnings("WeakerAccess")
+
 public class BaseOMTeslaContainerWrapper implements ITeslaConsumer {
 
     private final TileEntityElectric tile;
@@ -20,7 +22,11 @@ public class BaseOMTeslaContainerWrapper implements ITeslaConsumer {
 
     @Override
     public long givePower(long power, boolean simulated) {
-        return tile.receiveEnergy(facing, (int) power, simulated);
+        OMEnergyStorage storage = (OMEnergyStorage) tile.getCapability(CapabilityEnergy.ENERGY, facing);
+        if (storage != null) {
+            return storage.receiveEnergy((int) power, simulated);
+        }
+        return 0;
     }
 
 }
