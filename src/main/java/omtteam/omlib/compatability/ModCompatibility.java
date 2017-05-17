df984940ec7bd3601890a7749bccb609d56dacc2
Keridos <2742845+Keridos@users.noreply.github.com>
2017-05-18 01:09:27 +0200

    see https://github.com/OpenModularTurretsTeam/OpenModularTurrets/issues/322

--- src/main/java/omtteam/omlib/compatability/ModCompatibility.java
+++ src/main/java/omtteam/omlib/compatability/ModCompatibility.java
@@ -16,6 +16,7 @@ public class ModCompatibility {
     public static final String IC2ModId = "ic2";
     public static final String TeslaModId = "tesla";
     public static final String CoFHApiModId = "CoFHAPI";
+    public static final String OCModID = "opencomputers";
 
     public static void checkForMods() {
 
