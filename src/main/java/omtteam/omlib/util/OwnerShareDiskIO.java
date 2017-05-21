30a1ed5811dfad02598b9e102d9fcf73c0b28cb6
Keridos <fuffzehn@gmail.com>
2017-05-21 11:02:54 +0200

    some changes from 1.10.2

--- src/main/java/omtteam/omlib/util/OwnerShareDiskIO.java
+++ src/main/java/omtteam/omlib/util/OwnerShareDiskIO.java
@@ -18,43 +18,45 @@ import java.util.HashMap;
  * This Class
  */
 public class OwnerShareDiskIO {
-        public static void saveToDisk(OwnerShareHandler input) {
-            Path path = Paths.get(DimensionManager.getCurrentSaveRootDirectory().toString() + "/omlib/");
-            Path fullpath = Paths.get(DimensionManager.getCurrentSaveRootDirectory().toString() + "/omlib/OwnerShare.sav");
-            try {
-                if (Files.notExists(path)) {
-                    if (path.toFile().mkdir()) {
-                        throw new Exception("Failed to create dir");
-                    }
-                }
-                FileOutputStream saveFile = new FileOutputStream(fullpath.toFile());
-                ObjectOutputStream save = new ObjectOutputStream(saveFile);
-                save.writeObject(input.getOwnerShareMap());
-                save.close();
-                saveFile.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-                try {
-                    Files.deleteIfExists(fullpath);
-                } catch (Exception exception) {
-                    exception.printStackTrace();
+    public static void saveToDisk(OwnerShareHandler input) {
+        Path path = Paths.get(DimensionManager.getCurrentSaveRootDirectory().toString() + "/omlib/");
+        Path fullpath = Paths.get(DimensionManager.getCurrentSaveRootDirectory().toString() + "/omlib/OwnerShare.sav");
+        try {
+            if (Files.notExists(path)) {
+                if (path.toFile().mkdir()) {
+                    throw new Exception("Failed to create dir");
                 }
             }
+            FileOutputStream saveFile = new FileOutputStream(fullpath.toFile());
+            ObjectOutputStream save = new ObjectOutputStream(saveFile);
+            save.writeObject(input.getOwnerShareMap());
+            save.close();
+            saveFile.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                Files.deleteIfExists(fullpath);
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
         }
+    }
 
-        public static HashMap<Player, ArrayList<Player>>  loadFromDisk() {
-            HashMap<Player, ArrayList<Player>>  input = null;
-            try {
-                Path fullpath = Paths.get(DimensionManager.getCurrentSaveRootDirectory().toString() + "/omlib/OwnerShare.sav");
-                FileInputStream saveFile = new FileInputStream(fullpath.toFile());
-                ObjectInputStream save = new ObjectInputStream(saveFile);
+    public static HashMap<Player, ArrayList<Player>> loadFromDisk() {
+        HashMap<Player, ArrayList<Player>> input = new HashMap<>();
+        try {
+            Path fullpath = Paths.get(DimensionManager.getCurrentSaveRootDirectory().toString() + "/omlib/OwnerShare.sav");
+            FileInputStream saveFile = new FileInputStream(fullpath.toFile());
+            ObjectInputStream save = new ObjectInputStream(saveFile);
+            if (save.readObject() instanceof HashMap) {
                 input = (HashMap<Player, ArrayList<Player>>) save.readObject();
-                save.close();
-                saveFile.close();
-            } catch (Exception e) {
-                e.printStackTrace();
             }
-            return input;
+            save.close();
+            saveFile.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        return input;
+    }
 
 }
