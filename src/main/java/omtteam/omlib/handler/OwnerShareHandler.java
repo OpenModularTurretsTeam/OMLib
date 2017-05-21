a37b99c56bf45b09731391e9080d2769b033f63d
Keridos <2742845+Keridos@users.noreply.github.com>
2017-05-21 03:08:00 +0200

    first commit for share ownership command

---
+++ src/main/java/omtteam/omlib/handler/OwnerShareHandler.java
@@ -0,0 +1,101 @@
+package omtteam.omlib.handler;
+
+import omtteam.omlib.util.Player;
+
+import javax.annotation.Nullable;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Keridos on 17/05/17.
+ * This Class
+ */
+public class OwnerShareHandler implements Serializable {
+    private static OwnerShareHandler instance;
+    private HashMap<Player, ArrayList<Player>> ownerShareMap;
+
+    public OwnerShareHandler() {
+        ownerShareMap = new HashMap<>();
+    }
+
+    public static OwnerShareHandler getInstance() {
+        if (instance == null) {
+            instance = new OwnerShareHandler();
+        }
+        return instance;
+    }
+
+    public HashMap<Player, ArrayList<Player>> getOwnerShareMap() {
+        return ownerShareMap;
+    }
+
+    public void setOwnerShareMap(HashMap<Player, ArrayList<Player>> ownerShareMap) {
+        this.ownerShareMap = ownerShareMap;
+    }
+
+    @Nullable
+    public Map.Entry<Player, ArrayList<Player>> getEntryFromName(String name) {
+        for (Map.Entry<Player, ArrayList<Player>> entry : ownerShareMap.entrySet()) {
+            if (entry.getKey().getName().equals(name)) {
+                return entry;
+            }
+        }
+        return null;
+    }
+
+    @Nullable
+    private Map.Entry<Player, ArrayList<Player>> getEntry(Player player) {
+        for (Map.Entry<Player, ArrayList<Player>> entry : ownerShareMap.entrySet()) {
+            if (entry.getKey().equals(player)) {
+                return entry;
+            }
+        }
+        return null;
+    }
+
+    public void addSharePlayer(Player owner, Player sharePlayer) {
+        Map.Entry<Player, ArrayList<Player>> entryFound = null;
+        for (Map.Entry<Player, ArrayList<Player>> entry : ownerShareMap.entrySet()) {
+            if (entry.getKey().equals(owner)) {
+                entryFound = entry;
+            }
+        }
+        if (entryFound == null) {
+            ArrayList<Player> list = new ArrayList<>();
+            list.add(sharePlayer);
+            ownerShareMap.put(owner, list);
+        } else {
+            if (!entryFound.getValue().contains(sharePlayer)) {
+                entryFound.getValue().add(sharePlayer);
+            }
+        }
+    }
+
+    public void removeSharePlayer(Player owner, Player sharePlayer) {
+        Map.Entry<Player, ArrayList<Player>> entryFound = null;
+        for (Map.Entry<Player, ArrayList<Player>> entry : ownerShareMap.entrySet()) {
+            if (entry.getKey().equals(owner)) {
+                entryFound = entry;
+            }
+        }
+        if (entryFound != null) {
+            if (entryFound.getValue().contains(sharePlayer)) {
+                entryFound.getValue().remove(sharePlayer);
+            }
+        }
+    }
+
+    public boolean isPlayerSharedOwner(Player owner, Player shareCheck) {
+        Map.Entry<Player, ArrayList<Player>> entry = getEntry(owner);
+        if (entry != null) {
+            for (Player player : entry.getValue()) {
+                if (player.equals(shareCheck)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
