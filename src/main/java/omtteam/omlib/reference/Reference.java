4f4d0efa683b3e5fec7fcc4abcd8841107d9e641
Keridos <2742845+Keridos@users.noreply.github.com>
2017-05-28 23:21:27 +0200

    derp derp derp

--- src/main/java/omtteam/omlib/reference/Reference.java
+++ src/main/java/omtteam/omlib/reference/Reference.java
@@ -3,6 +3,6 @@ package omtteam.omlib.reference;
 public class Reference {
     public static final String MOD_ID = "omlib";
     public static final String NAME = "OMLib";
-    public static final String VERSION = "3.0.0-67";
+    public static final String VERSION = "@VERSION@";
     public static final String DEPENDENCIES = "after:IC2";
 }
