86b2cbde64962caeb1dcb4314b537fcdc10bcb90
Keridos <2742845+Keridos@users.noreply.github.com>
2017-06-01 00:43:51 +0200

    fixing a typo

--- src/main/java/omtteam/omlib/handler/OwnerShareHandler.java
+++ src/main/java/omtteam/omlib/handler/OwnerShareHandler.java
@@ -90,7 +90,7 @@ public class OwnerShareHandler implements Serializable {
                 }
             }
         }
-        OMLibNetworkingHandler.sendMessgeToAllPlayers(new MessageSetSharePlayerList(this));
+        OMLibNetworkingHandler.sendMessageToAllPlayers(new MessageSetSharePlayerList(this));
     }
 
     public void removeSharePlayer(Player owner, Player sharePlayer, @Nullable ICommandSender sender) {
@@ -120,7 +120,7 @@ public class OwnerShareHandler implements Serializable {
         } else if (sender != null) {
             sender.sendMessage(new TextComponentString("Could not remove player " + sharePlayer.getName() + " from your Share List!"));
         }
-        OMLibNetworkingHandler.sendMessgeToAllPlayers(new MessageSetSharePlayerList(this));
+        OMLibNetworkingHandler.sendMessageToAllPlayers(new MessageSetSharePlayerList(this));
     }
 
     public void printSharePlayers(Player owner, ICommandSender sender) {
