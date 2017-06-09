e1e38fb410cfd4a28c8079d3bc616c95ab85bbb5
Keridos <2742845+Keridos@users.noreply.github.com>
2017-06-10 01:23:42 +0200

    see https://github.com/OpenModularTurretsTeam/OpenModularTurrets/issues/323

---
+++ src/main/java/omtteam/omlib/compatability/opencomputers/AbstractOMDriver.java
@@ -0,0 +1,9 @@
+package omtteam.omlib.compatability.opencomputers;
+
+import li.cil.oc.api.prefab.DriverSidedTileEntity;
+
+/**
+ * Created by nico on 09/06/17.
+ */
+public abstract class AbstractOMDriver extends DriverSidedTileEntity {
+}
---
+++ src/main/java/omtteam/omlib/compatability/opencomputers/AbstractOMManagedEnvironment.java
@@ -0,0 +1,9 @@
+package omtteam.omlib.compatability.opencomputers;
+
+import li.cil.oc.api.prefab.ManagedEnvironment;
+
+/**
+ * Created by nico on 09/06/17.
+ */
+public class AbstractOMManagedEnvironment extends ManagedEnvironment {
+}
