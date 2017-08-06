bd148dd299bbd75afccc792b2136f1032c2d93eb
Keridos <2742845+Keridos@users.noreply.github.com>
2017-08-06 08:48:40 +0200

    some refactoring and added draw outline interfaces for mods to use

---
+++ src/main/java/omtteam/omlib/items/IDrawOutlineBase.java
@@ -0,0 +1,16 @@
+package omtteam.omlib.items;
+
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+
+/**
+ * Created by Keridos on 06/08/17.
+ * This Class
+ */
+public interface IDrawOutlineBase extends IDrawOutline {
+    @Nullable
+    public EnumFacing getBaseFacing(World world, BlockPos pos);
+}
