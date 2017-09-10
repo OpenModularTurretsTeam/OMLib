c3347840d6219de0c291d8aff25abbf0cf74e5b3
Keridos <2742845+Keridos@users.noreply.github.com>
2017-09-10 06:20:51 +0200

    some more api additions and changes.

--- src/main/java/omtteam/omlib/api/IDebugTile.java
+++ src/main/java/omtteam/omlib/api/IDebugTile.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 /**
  * Created by nico on 23/05/17.
- * This interface is for all box
+ * This interface is for all tile entities that should give back a debug string when right clicked with debug tool.
  */
 public interface IDebugTile {
     /**
