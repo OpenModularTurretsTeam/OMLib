e4bbdeb2c4fff82aab61576309ac50a7391f9b59
Keridos <2742845+Keridos@users.noreply.github.com>
2017-12-30 04:13:45 +0100

    updating forge and change owner command

--- src/main/java/omtteam/omlib/util/command/CommandChangeOwner.java
+++ src/main/java/omtteam/omlib/util/command/CommandChangeOwner.java
@@ -8,12 +8,15 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.WorldServer;
 import net.minecraftforge.common.DimensionManager;
+import omtteam.omlib.handler.ConfigHandler;
 import omtteam.omlib.tileentity.TileEntityOwnedBlock;
 
 import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.UUID;
 
 import static omtteam.omlib.util.PlayerUtil.addChatMessage;
+import static omtteam.omlib.util.PlayerUtil.getPlayerUUID;
 
 /**
  * Created by nico on 6/4/15.
@@ -56,9 +59,18 @@ public class CommandChangeOwner extends CommandBase {
 
             TileEntity tileEntity = worldserver.getTileEntity(new BlockPos(x, y, z));
             if (tileEntity instanceof TileEntityOwnedBlock) {
-                TileEntityOwnedBlock turret = (TileEntityOwnedBlock) tileEntity;
-                turret.setOwner(ownerName);
-                addChatMessage(sender, new TextComponentString("Block ownership has been updated"));
+                TileEntityOwnedBlock block = (TileEntityOwnedBlock) tileEntity;
+                UUID uuid = getPlayerUUID(ownerName);
+                if (uuid != null ) {
+                    block.setOwner(uuid.toString());
+                    block.setOwnerName(ownerName);
+                    addChatMessage(sender, new TextComponentString("Block ownership has been updated"));
+                } else if (ConfigHandler.offlineModeSupport) {
+                    block.setOwnerName(ownerName);
+                    addChatMessage(sender, new TextComponentString("Block ownership has been updated"));
+                } else {
+                    addChatMessage(sender, new TextComponentString("New owner not valid."));
+                }
             } else {
                 addChatMessage(sender, new TextComponentString("No ownable block was found at that location"));
             }
