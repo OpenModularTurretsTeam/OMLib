043bb4df14ca5dd641a9e688f26649f14182e159
Keridos <2742845+Keridos@users.noreply.github.com>
2018-02-04 03:52:05 +0100

    adding in fake sword

--- src/main/java/omtteam/omlib/init/OMLibItems.java
+++ src/main/java/omtteam/omlib/init/OMLibItems.java
@@ -2,6 +2,7 @@ package omtteam.omlib.init;
 
 import net.minecraft.item.Item;
 import net.minecraftforge.registries.IForgeRegistry;
+import omtteam.omlib.items.FakeSword;
 import omtteam.omlib.items.ItemDebugTool;
 
 import static omtteam.omlib.util.InitHelper.registerItem;
@@ -11,8 +12,11 @@ import static omtteam.omlib.util.InitHelper.registerItem;
  */
 public class OMLibItems {
     public static Item debugTool;
+    public static Item fakeSword;
 
     public static void init(IForgeRegistry<Item> registry) {
         debugTool = registerItem(new ItemDebugTool(), registry);
+        fakeSword = registerItem(new FakeSword(Item.ToolMaterial.DIAMOND), registry);
     }
+
 }
