8b0eacca852c33446de82465f8ece5d825a69095
Keridos <2742845+Keridos@users.noreply.github.com>
2018-08-30 00:52:39 +0200

    creating function to get armor from entity

--- src/main/java/omtteam/omlib/util/EntityUtil.java
+++ src/main/java/omtteam/omlib/util/EntityUtil.java
@@ -1,6 +1,8 @@
 package omtteam.omlib.util;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.registry.EntityEntry;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
@@ -13,5 +15,15 @@ public class EntityUtil {
     public static Class<? extends Entity> findClassById(String id) {
         EntityEntry entry = ForgeRegistries.ENTITIES.getValue(new ResourceLocation(id));
         return entry == null ? null : entry.getEntityClass();
+    }
+
+    public static int getEntityArmor(Entity entity) {
+        int armor = 0;
+        Iterable<ItemStack> iter = entity.getArmorInventoryList();
+        while (iter.iterator().hasNext()) {
+            ItemStack itemStack = iter.iterator().next();
+            armor += ((ItemArmor) itemStack.getItem()).damageReduceAmount;
+        }
+        return armor;
     }
 }
